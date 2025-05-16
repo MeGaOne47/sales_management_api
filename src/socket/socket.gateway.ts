262a9493f96b9702aaa8875d6a4c3688165cb046
Nguyễn Tấn Hùng <nguyentanhung9a1@gmail.com>
2025-05-16 12:35:14 +0700

    project initiation

---
+++ src/socket/socket.gateway.ts
@@ -0,0 +1,32 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*', // Hoặc domain frontend của bạn
+  },
+})
+export class SocketGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  handleConnection(client: Socket) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
+
+  @SubscribeMessage('chat')
+  handleChat(client: Socket, payload: any) {
+    console.log('Received chat:', payload);
+    this.server.emit('chat', payload); // Broadcast lại
+  }
+}
